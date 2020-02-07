7796c2be58108acf1d8e2ca2a8ce36cf3a5e39de
Ben Wilson and Meliah MacKay <ben@>
2020-02-06 20:46:48 -0800

    add a dark mode and toggleable facts

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,65 @@ $(document).ready(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
   });
+  
   $(".click2").click(function() {
     $("#walrus2").fadeToggle();
   });
+
   $(".click3").click(function() {
-    $("#otter").slideToggle();
+  $("#otter").slideToggle();
+  });
+
+
+  $(".click4").click(function() {
+    $("#fact4").toggle();
+    });
+
+  $(".click5").click(function() {
+    $("#fact5").toggle();
+    });
+
+  $(".click6").click(function() {
+    $("#fact6").toggle();
+    });
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("darkMode");
   });
 });
